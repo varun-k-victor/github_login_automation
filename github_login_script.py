01d0076e8ad29168ccc8a689e48568ab1bbe85d1
varun-k-victor <varun7victor@gmail.com>
2021-10-16 16:53:30 +0530

    create github_login_script.py

---
+++ github_login_script.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+def userin_username():
+    username=input("Enter username: ")
+    return username
+
+def userin_passwd():
+    password=input("Enter password: ")
+    return password
+
+USERNAME=userin_username()
+PASSWORD=userin_passwd()
+print("Code running....")
+sleep(3)
+
+driver=webdriver.Firefox()
+sleep(3)
+driver.get('https://github.com/login')
+sleep(3)
+username=driver.find_element_by_name('login')
+username.send_keys(USERNAME)
+sleep(3)
+password=driver.find_element_by_name('password')
+password.send_keys(PASSWORD)
+sleep(3)
+password.send_keys(Keys.ENTER)
