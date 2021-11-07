d75bacb639d8430d082b5c5c7306ac8a2c31d0c4
varun-k-victor <varun7victor@gmail.com>
2021-11-08 01:43:29 +0530

    minor improvement

--- github_login_script.py
+++ github_login_script.py
@@ -10,19 +10,22 @@ def userin_passwd():
     password=input("Enter password: ")
     return password
 
-USERNAME=userin_username()
-PASSWORD=userin_passwd()
-print("Code running....")
-sleep(3)
-
-driver=webdriver.Firefox()
-sleep(3)
-driver.get('https://github.com/login')
-sleep(3)
-username=driver.find_element_by_name('login')
-username.send_keys(USERNAME)
-sleep(3)
-password=driver.find_element_by_name('password')
-password.send_keys(PASSWORD)
-sleep(3)
-password.send_keys(Keys.ENTER)
+def main():
+	USERNAME=userin_username()
+	PASSWORD=userin_passwd()
+	print("Code running....")
+	sleep(3)
+	driver=webdriver.Firefox()
+	sleep(3)
+	driver.get('https://github.com/login')
+	sleep(3)
+	username=driver.find_element_by_name('login')
+	username.send_keys(USERNAME)
+	sleep(3)
+	password=driver.find_element_by_name('password')
+	password.send_keys(PASSWORD)
+	sleep(3)
+	password.send_keys(Keys.ENTER)
+	
+if __name__=="__main__":
+	main()
